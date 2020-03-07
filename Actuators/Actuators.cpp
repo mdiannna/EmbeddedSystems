b661f9ebe45ef4a90873ebe1bafcd2ff5f2b356e
Diana <mdiannna@yahoo.com>
2020-03-07 12:07:07 +0200

    Final project code cleanup

--- Actuators/Actuators.cpp
+++ Actuators/Actuators.cpp
@@ -4,13 +4,8 @@
 #include <stdio.h>
 #include "mystdio.h"
 #include "light.h"
-#include "mykeypad.h"
 
 
-/*  Arduino DC Motor Control - PWM | H-Bridge | L298N  -  Example 01
-
-    by Dejan Nedelkovski, www.HowToMechatronics.com
-*/
 
 #define enA 9
 #define in1 6
@@ -24,36 +19,12 @@ char * s;
 
 void setup() {
 	InitSerial();
-	//InitKeypad();
 	InitCar();
 	InitLight();
 	printf("Initialization done");
  s = (char *) malloc(10*sizeof(char));
 }
 
-
-
-// void loopTestMotor() {
-
-// 	if(cnt<500) {
-
-// 		int motorSpeedA = 100;
-// 		 analogWrite(enA, motorSpeedA);
-// 		 delay(20);
-// 	} else if(cnt <1000) {
-// 		  digitalWrite(in2, LOW);
-// 		  digitalWrite(in1, HIGH);
-
-// 		int motorSpeedA = 100;
-// 				 analogWrite(enA, motorSpeedA);
-// 				 delay(20);
-
-// 	}
-// 	else {
-// 		 analogWrite(enA, 0);
-// 	}
-// 	cnt++;
-// }
 
 
 void loop() {
@@ -103,28 +74,13 @@ void loop() {
 		 }
 		 printf("Car speed changed\n\r");
 	}
-	//
-	//printf("You typed!!!: %c\n\r", c );
 
-	//	String s ="";
-//	if (Serial.available() > 0) {
-//	char c = Serial.read();
-//
-////	char c = GetCharKeypad();
-//	printf("You typed: %c\n\r", c );
-//
-//
-//	delay(1000);
-//	 s = Serial.readString();
-//	printf("You typed: %s\n", s );
-//	}
-	//if(s=="forward") {
+
 	if(c=='f') {
 		MoveForward();
 		printf("Car Moves Forward\n\r");
 	}
 
-//	if(s=="backward") {
 	if(c=='b') {
 		MoveBackward();
 		printf("Car Moves Backward\n\r");
@@ -135,24 +91,18 @@ void loop() {
 			printf("Car Stops\n\r");
 		}
 
-//	if(s=="left") {
 	if(c=='l') {
 		TurnLeft();
 	}
 
-//	if(s=="right") {
 	if(c=='r') {
 		TurnRight();
 	}
 		
-	// * - Light on
-	//if(s=="lighton" && !IsLightOn()) {
 	if(c=='n') {
 		LightOn();
 	}
 
-	// # - Light off
-	//if(s=="lightof" && IsLightOn()) {
 	if(c=='t') {
 		LightOff();
 	}
